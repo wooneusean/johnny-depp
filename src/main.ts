e338c46099e63ee367f5042b41342a8f00d6677f
snappymob-woon <146707643+snappymob-woon@users.noreply.github.com>
2024-09-01 19:01:26 +0800

    chore: remove unused import

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,4 @@
-import "reflect-metadata";
 import JohnnyDepp from "../lib/JohnnyDepp";
-import { ServiceOneController } from "./controllers/service-one.controller.ts";
 import ServiceThree from "./services/service-three.service.ts";
 
 JohnnyDepp.get(ServiceThree).test();
